c3d7dd47f8bf1d4dc84e4bfc1bbfc9af49b5ac23
anup <anuppal602@gmail.com>
2022-01-30 21:26:29 +0530

    m

--- src/Cpp/codechef/subarrayPermutation.cpp
+++ src/Cpp/codechef/subarrayPermutation.cpp
@@ -3,20 +3,33 @@ using namespace std;
 
 int main(){
     int t;cin>>t;
-    while(t--){
-        int n,k;cin>>n>>k;
-        if(n>1 && k==1){cout<<-1<<endl;
-        continue;}
-
-        vector<int >v(n);
-        iota(v.begin(),v.end(),1);
-        reverse(v.end()-(n-k+1),v.end());
-        for(int x:v){
-            cout<<x<<" ";
-        }
-        cout<<endl;
-
-
+    
+    string s ="";
+    for (size_t i = 0; i < t; i++)
+    {
+        /* code */
+        cin>>s;
     }
-    return 0;
+    cout<<s;
+    
+    // while(t--){
+    //     int n,k;cin>>n>>k;
+    //     if(n>1 && k==1){cout<<-1<<endl;
+    //     continue;}
+
+    //     vector<int >v(n);
+    //     iota(v.begin(),v.end(),1);
+    //     reverse(v.end()-(n-k+1),v.end());
+    //     for(int x:v){
+    //         cout<<x<<" ";
+    //     }
+    //     cout<<endl;
+
+
+    // }
+    // return 0;
+
+
+
+
 }
