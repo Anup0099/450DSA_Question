48bfb5e3ad160cb0900b7009226b99ed16334828
anup <anuppal602@gmail.com>
2022-02-16 21:02:27 +0530

    badminton

--- src/Cpp/dp/trees.cpp
+++ src/Cpp/dp/trees.cpp
@@ -1,17 +1,29 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define fastio  
-using namespace std;
-class node{
-    int data;
-    node* left;
-    node* right;
-    node(int d){
-        this->data=d;
-        this->left=NULL;
-        this->right=NULL;
-    }
-};
-int main(){
 
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        int arr[n];
+        int arr1[n + 1];
+        for (size_t i = 0; i < n + 1; i++)
+        {
+            arr1[i] = 0;
+        }
+
+        for (size_t i = 1; i <= n; i++)
+        {
+            cin >> arr[i];
+            arr1[i] = arr1[i - 1] + arr[i];
+        }
+        sort(arr1, arr1 + n + 1);
+        cout << arr1[n] + x << endl;
+    }
 }
