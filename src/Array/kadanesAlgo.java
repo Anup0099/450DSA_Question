f86ea473e7158b549fe58d32cfc4b99988482ec7
anup <anuppal602@gmail.com>
2022-02-19 11:36:42 +0530

    treesss

--- src/Array/kadanesAlgo.java
+++ src/Array/kadanesAlgo.java
@@ -1,2 +1,35 @@
-package Array;public class kadanesAlgo {
+package Array;
+
+import java.util.ArrayList;
+
+public class kadanesAlgo {
+     public static int subarray(int[] nums,
+                               ArrayList<Integer> arr) {
+        int sum = 0;
+        int s=0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max) {
+                max = sum;
+                arr.add(s);
+                arr.add(i);
+            }
+            if (sum < 0) {
+                sum = 0;
+                s = i+1;
+
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        ArrayList<Integer>arr = new ArrayList<>();
+        System.out.println(subarray(nums,arr));
+        for (int i = arr.get(0); i <=arr.get(1); i++) {
+            System.out.println(nums[i]+"");
+        }
+    }
 }
--- src/Linkedlist/doubly.java
+++ src/Linkedlist/doubly.java
@@ -1,2 +1,43 @@
-package Linkedlist;public class doubly {
+package Linkedlist;
+
+public class doubly {
+    public static class node {
+        int data;
+        node prev;
+        node next;
+
+        public node(int data) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
+
+    static void insert(node head,
+                       int data) {
+        node temp = new node(data);
+        temp.next = head;
+        head.prev = temp;
+        head = temp;
+    }
+
+    static void print(node head) {
+        node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        node node1 = new node(10);
+        node head = node1;
+        print(head);
+        insert(head, 11);
+        print(head);
+
+
+    }
 }
