1d37d4e085cc47ca6dc27b8e060a702916e1dca2
anup <anuppal602@gmail.com>
2022-02-22 15:55:27 +0530

    binary

---
+++ src/aabstract.java
@@ -0,0 +1,39 @@
+
+
+abstract  class Human {
+    public abstract void eat();
+    public void  walk(){
+
+    }
+}
+class Man extends  Human{
+    public void eat(){
+
+    }
+}
+public class aabstract {
+    public static void main(String[] args) {
+        b obj = new b();
+
+    }
+}
+//why abstract
+class a{
+    public a(){
+        System.out.println("heeloa");
+    }
+}
+class b extends  a{
+    public b(){
+        System.out.println("heelob");
+    }
+}
+class Printer{
+    public void show(Integer i){
+        b obj = new b();
+        System.out.println(obj);
+    }
+//    public void show(Double i){
+//        System.out.println(i);
+//    }
+}
