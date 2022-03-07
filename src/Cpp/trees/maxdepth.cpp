025214245c82761c47efecc0fe4b1d6658743186
anup <anuppal602@gmail.com>
2022-03-07 21:45:46 +0530

    duplicate

--- src/Cpp/trees/maxdepth.cpp
+++ src/Cpp/trees/maxdepth.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
-int main(){
+bool containsDuplicate(vector<int> &nums)
+{
+    map<int,int>m;
     
+    
+    for (int i = 0; i < nums.size();i++){
+       if(m.find(nums[i]) != m.end()){
+           return true;
+       } else{
+           m.insert({nums[i],i});
+
+       }
+    }
+    return false;
+    
+
+}
+int main()
+{
+    vector<int> numbers={1,2,3,4};
+    cout<<containsDuplicate(numbers)<<endl;
 }
