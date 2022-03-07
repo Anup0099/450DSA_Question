029a8e56daab731964aaee9b3db007f35b8527c0
anup <anuppal602@gmail.com>
2022-03-07 20:44:23 +0530

    bit++

---
+++ src/Cpp/codechef/beautifulMatricx.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 3/3/2022.
+//
+
+#include "beautifulMatricx.h"
---
+++ src/Cpp/codechef/beautifulMatricx.h
@@ -0,0 +1,16 @@
+//
+// Created by Asus on 3/3/2022.
+//
+
+#ifndef CPP_BEAUTIFULMATRICX_H
+#define CPP_BEAUTIFULMATRICX_H
+
+
+
+class beautifulMatricx {
+
+};
+
+
+
+#endif //CPP_BEAUTIFULMATRICX_H
