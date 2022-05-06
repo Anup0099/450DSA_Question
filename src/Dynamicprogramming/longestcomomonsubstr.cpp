5b242ba1adb79f6edc6d38d54c61c3eb60c5a421
anup <anuppal602@gmail.com>
2022-05-06 23:20:55 +0530

    cc

---
+++ src/Dynamicprogramming/longestcomomonsubstr.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestCommonSubsequence(string text1, string text2)
+{
+    int n = text1.length();
+    int m = text2.length();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (text1[i - 1] == text2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+
+int main()
+{
+    int t = longestCommonSubsequence("abcde", "ace");
+    cout << t;
+}
