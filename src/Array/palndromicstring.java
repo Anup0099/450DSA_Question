9aaae6b36ed4b0f24063a4c33fa4a34132729a23
anup <anuppal602@gmail.com>
2022-05-23 00:42:51 +0530

    cc

--- src/Array/palndromicstring.java
+++ src/Array/palndromicstring.java
@@ -1,2 +1,44 @@
-package Array;public class palndromicstring {
+package Array;
+
+import java.util.Scanner;
+
+public class palndromicstring {
+    static int palindromeIndex(String s){
+        int palindromeIndex = -1;
+        int n = s.length();
+        for (int i = 0; i < n/2; i++) {
+            if (s.charAt(i) != s.charAt(n-i-1)) {
+                if(i+1 < n){
+                    boolean plandrome = isValidPalindrome(s.substring((i+1), n));
+                    if(plandrome){
+                        return i;
+                    }
+                    return  n-i-1;
+                }
+            }
+        }
+        return palindromeIndex;
+    }
+
+    private static boolean isValidPalindrome(String substring) {
+        int n = substring.length();
+        for (int i = 0; i < n/2; i++) {
+            if (substring.charAt(i) != substring.charAt(n-i-1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for (int i = 0; i < q; i++) {
+            String s = in.next();
+            int result = palindromeIndex(s);
+            System.out.println(result);
+
+        }
+        in.close();
+    }
 }
