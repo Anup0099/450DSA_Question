8f18e30e1b944ef5167c44653b21de703eabff94
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2022-10-10 14:10:04 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+
+        int n=s.length();
+    if(n==1) return "";
+    for(int i=0;i<n/2;i++){
+        if(s[i]!='a'){
+            s[i]='a';
+            return s;
+        }
+    }
+    s[n-1]='b';
+    return s;
+    }
+};
