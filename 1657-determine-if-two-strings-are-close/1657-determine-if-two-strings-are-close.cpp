3ec682191033bd59d0379e85656e24757ddb8859
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2022-12-02 10:35:28 +0530

    Time: 269 ms (40.42%), Space: 20.5 MB (64.37%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+         int n1=word1.size(); 
+    int n2=word2.size(); 
+    if(n1!=n2) return false; 
+    vector<int> freq1(26,0); 
+    vector<int> freq2(26,0); 
+    for(int i=0;i<n1;i++){ 
+        freq1[word1[i]-'a']++; 
+        freq2[word2[i]-'a']++; 
+    } 
+    for(int i=0;i<26;i++){ 
+        if((freq1[i]==0 && freq2[i]!=0) || (freq1[i]!=0 && freq2[i]==0)) return false; 
+    } 
+    sort(freq1.begin(),freq1.end()); 
+    sort(freq2.begin(),freq2.end()); 
+    for(int i=0;i<26;i++){ 
+        if(freq1[i]!=freq2[i]) return false; 
+    } 
+    return true; 
+    }
+};
