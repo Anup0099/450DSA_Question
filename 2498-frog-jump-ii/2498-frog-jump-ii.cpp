d7368235eae5581178a622317cde2018f22b6d09
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2022-12-10 23:59:50 +0530

    Time: 154 ms (100.00%), Space: 65.4 MB (55.56%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxJump(vector<int>& A) {
+         int res = A[1] - A[0], n = A.size();
+        for (int i = 2; i < n; ++i)
+            res = max(res, A[i] - A[i - 2]);
+        return res;
+    }
+};
