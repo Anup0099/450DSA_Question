0e146928d8b2a294eaa5d861a8701169d403dd40
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2022-12-27 22:57:11 +0530

    Time: 188 ms (96.48%), Space: 86.8 MB (67.47%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n = capacity.size();
+        int count = 0;
+        vector<int> vec(n);
+        
+        for(int i = 0; i<n; i++) {
+            vec[i] = capacity[i] - rocks[i];
+        }
+        
+        sort(begin(vec), end(vec));
+        
+        for(int i = 0; i<n; i++) {
+            if(additionalRocks >= vec[i]) {
+                additionalRocks -= vec[i];
+                count++;
+            } else
+                break;
+        }
+        
+        return count;
+    }
+};
