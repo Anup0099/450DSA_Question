73025a86a06f87b98df842bc7771eaa720efdfcf
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2023-01-01 18:36:25 +0530

    Time: 151 ms (36.36%), Space: 51.4 MB (9.09%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+   unordered_set<int> getPrimeFactors(int num)
+{
+    unordered_set<int> primeFactors;
+    while (num % 2 == 0)
+    {
+        primeFactors.insert(2);
+        num /= 2;
+    }
+    for (int i = 3; i <= sqrt(num); i += 2)
+    {
+        while (num % i == 0)
+        {
+            primeFactors.insert(i);
+            num /= i;
+        }
+    }
+    if (num > 2)
+    {
+        primeFactors.insert(num);
+    }
+    return primeFactors;
+}
+int distinctPrimeFactors(vector<int> &nums)
+{
+    unordered_set<int> primeFactors;
+    for (auto num : nums)
+    {
+        unordered_set<int> current = getPrimeFactors(num);
+        primeFactors.insert(current.begin(), current.end());
+    }
+    return primeFactors.size();
+}
+
+};
