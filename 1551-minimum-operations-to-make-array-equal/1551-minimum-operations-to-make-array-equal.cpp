34b86ae7e743d98d6e066df7b00e21820e6cca4a
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2023-01-23 13:42:30 +0530

    Time: 4 ms (45.67%), Space: 6 MB (20.40%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int minOperations(int n) {
-        int ops = 0;
-    for (int i = 0; i < n / 2; i++)
+       int mid = n / 2;
+    if (n % 2 == 0)
     {
-        int x = 2 * i + 1;
-        ops += (n - x);
+        return mid * mid;
     }
-    return ops;
+    else
+    {
+        return mid * (mid + 1);
+    }
+    return -1;
     }
 };
