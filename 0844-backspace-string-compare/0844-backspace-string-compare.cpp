e0ce84038ae0a9814fd649e9ddefb979ba6f9b70
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2023-01-27 20:26:25 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (98.59%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+         string a, b;
+    for (char c : s)
+    {
+        if (c != '#')
+        {
+            a.push_back(c);
+        }
+        else if (a.size() && c == '#')
+        {
+            a.pop_back();
+        }
+    }
+    for (char c : t)
+    {
+        if (c != '#')
+        {
+            b.push_back(c);
+        }
+        else if (b.size() && c == '#')
+        {
+            b.pop_back();
+        }
+    }
+    return a == b;
+    }
+};
