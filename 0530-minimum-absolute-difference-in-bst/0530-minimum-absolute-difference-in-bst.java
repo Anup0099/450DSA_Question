c911137e0d5a598d076be7fde4a3479f0e1ae69b
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2023-03-01 23:55:44 +0530

    Time: 6 ms (9.43%), Space: 43.2 MB (13.32%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (q.isEmpty() == false) {
+            TreeNode temp = q.poll();
+            list.add(temp.val);
+            if (temp.left != null) {
+                q.add(temp.left);
+            }
+            if (temp.right != null) {
+                q.add(temp.right);
+            }
+
+        }
+        Collections.sort(list);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < list.size() - 1; i++) {
+            min = Math.min(min, list.get(i + 1) - list.get(i));
+        }
+        return min;
+    }
+}
