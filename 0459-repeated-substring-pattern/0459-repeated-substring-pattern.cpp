19e75b3121f52e2ee401a71f9c03f5c6ca06018b
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2023-03-03 09:58:39 +0530

    Time: 19 ms (77.30%), Space: 15.3 MB (42.67%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+       int n = s.size();
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            string sub = s.substr(0, i);
+            int j = i;
+            while (j < n)
+            {
+                if (s.substr(j, i) != sub)
+                    break;
+                j += i;
+            }
+            if (j == n)
+                return true;
+        }
+    }
+    return false; 
+    }
+};
