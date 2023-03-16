9d1314eca44c5ab09885dd836159f23c7d15f2d6
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2023-03-16 20:45:26 +0530

    Time: 31 ms (36.63%), Space: 26.1 MB (78.64%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,28 +11,31 @@
  */
 class Solution {
 public:
-  TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder)
+    TreeNode *solve(vector<int> &inorder, vector<int> &postorder, int inStart, int inEnd, int postStart, int postEnd)
+{
+    if (inStart > inEnd || postStart > postEnd)
     {
-        if (inorder.size() != postorder.size())
-        {
-            return NULL;
-        }
-        map<int, int> hm;
-        for (int i = 0; i < inorder.size(); i++)
-        {
-            hm[inorder[i]] = i;
-        }
-        return buildTreePostIN(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1, hm);
+        return NULL;
     }
-    TreeNode *buildTreePostIN(vector<int> &inorder, int is, int ie, vector<int> &postorder, int ps, int pe, map<int, int> &hm)
+    int rootVal = postorder[postEnd];
+    TreeNode *root = new TreeNode(rootVal);
+    int k = 0;
+    for (int i = inStart; i <= inEnd; i++)
     {
-        if (ps > pe || is > ie)
-            return NULL;
-        TreeNode *root = new TreeNode(postorder[pe]);
-        int inRoot = hm[postorder[pe]];
-        int numsLeft = inRoot - is;
-        root->left = buildTreePostIN(inorder, is, inRoot - 1, postorder, ps, ps + numsLeft - 1, hm);
-        root->right = buildTreePostIN(inorder, inRoot + 1, ie, postorder, ps + numsLeft, pe - 1, hm);
-        return root;
+        if (inorder[i] == rootVal)
+        {
+            k = i;
+            break;
+        }
+    }
+    root->left = solve(inorder, postorder, inStart, k - 1, postStart, postStart + k - (inStart + 1));
+    root->right = solve(inorder, postorder, k + 1, inEnd, postStart + k - inStart, postEnd - 1);
+    return root;
+}
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+         int n = inorder.size();
+    int inStart = 0, inEnd = n - 1;
+    int postStart = 0, postEnd = n - 1;
+    return solve(inorder, postorder, inStart, inEnd, postStart, postEnd);
     }
 };
