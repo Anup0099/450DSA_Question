4027ca7a8391c26d975efbd9137cbec26ee05952
Anup0099 <75351645+Anup0099@users.noreply.github.com>
2023-03-30 00:39:37 +0530

    Time: 2702 ms (5.11%), Space: 589.8 MB (6.44%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    int n;
-    int solve(vector<int> &satisfaction, int i, int time, vector<vector<int>> &t) {
-        
-        if(i == n)
-            return 0;
-        
-        if(t[i][time] != -1)
-            return t[i][time];
-        
-        int include = satisfaction[i]*time + solve(satisfaction, i+1, time+1, t);
-        int exclude = solve(satisfaction, i+1, time, t);
-        
-        return t[i][time] = max(include, exclude);
-        
-    }
-    
     int maxSatisfaction(vector<int>& satisfaction) {
-        n = satisfaction.size();
-        
-        sort(begin(satisfaction), end(satisfaction));
-        
-        vector<vector<int>> t(n+1, vector<int>(n+1, -1));
-        
-        return solve(satisfaction, 0, 1, t);
+        int n = satisfaction.size();
+    sort(satisfaction.begin(), satisfaction.end());
+    // int time = 1;
+    // int index = 0;
+    // int sum = 0;
+    // vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+    // return solve(satisfaction, index, time,dp);
+    // bottom up approach
+    vector<vector<long long>> dp(5001, vector<long long>(5001, INT_MIN));
+    for (int i = 0; i < 5001; i++)
+    {
+        // time =0 pe nahi suru hoga khana banana
+        dp[i][0] = 0;
+    }
+    dp[0][1] = satisfaction[0];
+    for (int i = 1; i < n; i++)
+    {
+        for (int time = 1; time <= n; time++)
+        {
+            long long include = satisfaction[i] * time + dp[i - 1][time - 1];
+            long long exclude = dp[i - 1][time];
+            dp[i][time] = max(include, exclude);
+        }
+    }
+    // asks for any time
+    long long ans = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        ans = max(ans, dp[n - 1][i]);
+    }
+    return ans; 
     }
 };
