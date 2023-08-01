187b8de3b09dddea67ce8f5383af838eeb85319b
ANUP PAL <75351645+Anup0099@users.noreply.github.com>
2023-08-01 20:39:16 +0530

    Time: 172 ms (5.68%), Space: 5.8 MB (87.86%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
-    int trailingZeroes(int n) {
-       int count = 0;
-    int i = 5;
-    while (n / i >= 1)
+   int trailingZeroes(int n)
+{
+    int count = 0;
+    for (int i = 1; i <= n; i++)
     {
-        count += n / i;
-        i *= 5;
+        // run a loop from 1 to n and count the number of 5s in each number
+        int j = i;
+        while (j % 5 == 0)
+        {
+            //if the number is divisible by 5, increase the count and divide the number by 5
+            count++;
+            j /= 5;
+            cout<<j<<endl;
+            // keep dividing the number by 5 until it is divisible by 5
+        }
     }
+
     return count;
-    }
+}
+int trailingZero(int n)
+{
+    return trailingZeroes(n);
+}
 };
