adfea560d0d1294f5fd05ce0843f35fd79fc1d82
ANUP PAL <75351645+Anup0099@users.noreply.github.com>
2023-09-03 23:42:37 +0530

    Time: 84 ms (92.31%), Space: 62.6 MB (89.91%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    unordered_set<string> st;
+    int t[51];
+    
+    int n;
+    
+    int solve(int idx, string &s) {
+        
+        if(idx >= n)
+            return 0;
+        
+        if(t[idx] != -1)
+            return t[idx];
+        
+        string currStr = "";
+        int minExtra   = n;
+        
+        for(int i = idx; i<n; i++) {
+            
+            currStr.push_back(s[i]);
+            
+            int currExtra = (st.find(currStr) == st.end()) ? currStr.length() : 0;
+            
+            int nextExtra = solve(i+1, s);
+            
+            int totalExtra = currExtra + nextExtra;
+            
+            minExtra = min(minExtra, totalExtra);
+            
+        }
+        
+        return t[idx] = minExtra;
+        
+    }
+    
+    int minExtraChar(string s, vector<string>& dictionary) {
+        n = s.length();
+        
+        memset(t, -1, sizeof(t));
+        
+        for(string &word : dictionary)
+            st.insert(word);
+        
+        return solve(0, s);
+    }
+};
