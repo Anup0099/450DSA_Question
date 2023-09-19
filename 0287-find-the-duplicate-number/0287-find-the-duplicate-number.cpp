a5e9d9e6362491b902621f38471849280ef80918
ANUP PAL <75351645+Anup0099@users.noreply.github.com>
2023-09-19 22:47:07 +0530

    Time: 83 ms (67.10%), Space: 61.5 MB (30.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-// the only one repeated number in nums return this repeated number xor with all the elements to array me dulicate elements nahi rahenge na wo element kaise milenge seedha map me daalo to sahi rahega jiska bhi count >1 hoga use print  kara denge
-        map<int,int>map;
-        for(int it:nums)
-        {
-            map[it]++;
-        }
-        int ans=0;
-        for(auto it:map)
-        {
-            if(it.second>1)
-            {
-                ans = it.first;
-                break;
-                
-            }
-        }
-        return ans;
+        int n = nums.size();
+    int fast = nums[0];
+    int slow = nums[0];
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+    while (slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    slow = nums[0];
+    while (slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return fast;
     }
 };
