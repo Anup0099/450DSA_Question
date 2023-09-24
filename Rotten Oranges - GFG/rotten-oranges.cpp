3b473ccdf837f0683b5b164ed25d4c05dc8fc766
ANUP PAL <75351645+Anup0099@users.noreply.github.com>
2023-09-24 16:49:14 +0530

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        // Code here
+         int n = grid.size();
+    int m = grid[0].size();
+    int time = 0;
+
+    queue<pair<pair<int, int>, int>> q;
+    int vis[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] == 2)
+            {
+                q.push({{i, j}, 0});
+
+                vis[i][j] = 2;
+            }else {
+                vis[i][j]=0;
+            }
+        }
+    }
+
+    int delrow[] = {-1, 0, 1, 0};
+    int delcol[] = {0, 1, 0, -1};
+
+    while (!q.empty())
+    {
+        int row = q.front().first.first;
+        int col = q.front().first.second;
+        int t = q.front().second;
+        q.pop();
+        time = max(time, t);
+        
+        
+        for (int i = 0; i < 4; i++)
+        {
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+
+            if (nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and grid[nrow][ncol] == 1 and vis[nrow][ncol] != 2)
+            {
+                q.push({{nrow, ncol}, t + 1});
+                vis[nrow][ncol] = 2;
+            }
+        }
+    }
+    // check if all oranges cant be reached
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] == 1 and vis[i][j] != 2)
+            {
+                return -1;
+            }
+        }
+    }
+    return time;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
