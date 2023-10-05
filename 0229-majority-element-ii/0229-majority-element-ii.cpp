9e00fd185ddf62e62e0d0085dca733f20ddf55e6
ANUP PAL <75351645+Anup0099@users.noreply.github.com>
2023-10-05 23:38:49 +0530

    Time: 15 ms (14.75%), Space: 16.5 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-       vector<int> ans;
+         vector<int> ans;
     map<int, int> mp;
     int n = nums.size();
     for (int i = 0; i < n; i++)
@@ -16,7 +16,5 @@ public:
         }
     }
     return ans;
-    //time complexity:total time complexity is O(nlogn) because we are using a map and map has a time complexity of O(logn) for insertion and deletion
-    //space complexity: O(n) because we are using a map 
     }
 };
