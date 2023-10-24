fc7278285c1b22edb9baf57c0a111c0d5330e540
ANUP PAL <75351645+Anup0099@users.noreply.github.com>
2023-10-24 10:12:59 +0530

    Time: 2 ms (33.69%), Space: 6.5 MB (13.76%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==1 )
+            return true;
+        if(n==0)return false;
+        while(n%4==0)
+        {
+            n=n/4;
+            if(n==1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
