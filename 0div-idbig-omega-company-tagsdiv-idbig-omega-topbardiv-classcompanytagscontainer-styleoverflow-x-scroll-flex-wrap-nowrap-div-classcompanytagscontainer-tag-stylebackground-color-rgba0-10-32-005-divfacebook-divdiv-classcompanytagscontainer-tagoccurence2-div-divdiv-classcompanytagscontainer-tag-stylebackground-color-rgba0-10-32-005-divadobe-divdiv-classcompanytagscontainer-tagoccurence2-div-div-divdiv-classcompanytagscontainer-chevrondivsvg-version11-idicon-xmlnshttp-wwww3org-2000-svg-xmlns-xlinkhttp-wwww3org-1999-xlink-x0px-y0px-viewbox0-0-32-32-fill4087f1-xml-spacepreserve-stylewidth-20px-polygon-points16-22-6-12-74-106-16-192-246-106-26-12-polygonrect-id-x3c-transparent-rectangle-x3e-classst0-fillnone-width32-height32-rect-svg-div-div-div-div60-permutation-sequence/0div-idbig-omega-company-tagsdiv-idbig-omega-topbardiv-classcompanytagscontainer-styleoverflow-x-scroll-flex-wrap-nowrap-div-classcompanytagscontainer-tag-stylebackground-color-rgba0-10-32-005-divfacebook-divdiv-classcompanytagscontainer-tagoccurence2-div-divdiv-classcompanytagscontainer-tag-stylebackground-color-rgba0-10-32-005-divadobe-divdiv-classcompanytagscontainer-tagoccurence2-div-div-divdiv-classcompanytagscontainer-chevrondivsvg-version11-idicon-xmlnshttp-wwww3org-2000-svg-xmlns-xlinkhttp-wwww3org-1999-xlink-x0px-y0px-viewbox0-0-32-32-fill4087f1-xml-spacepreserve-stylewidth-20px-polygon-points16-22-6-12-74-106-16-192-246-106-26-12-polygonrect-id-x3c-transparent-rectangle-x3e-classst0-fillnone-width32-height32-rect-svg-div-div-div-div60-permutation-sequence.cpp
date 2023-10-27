0940b571fe0dbe79376ee6eedc9889390635498d
ANUP PAL <75351645+Anup0099@users.noreply.github.com>
2023-10-27 12:16:37 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.92%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div60-permutation-sequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div60-permutation-sequence.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+        string getPermutation(int n, int k) {
+            string ans = "";
+    vector<int> number;
+    int fact = 1;
+    for (int i = 1; i < n; i++)
+    {
+        fact = fact * i;
+        number.push_back(i);
+    }
+    number.push_back(n);
+    k=k-1;
+    while (true)
+    {
+        ans += to_string(number[k / fact]);
+        number.erase(number.begin() + k / fact);
+        if (number.size() == 0)
+        {
+            break;
+        }
+        k = k % fact;
+        fact = fact / number.size();
+    }
+    return ans;
+        }
+};
